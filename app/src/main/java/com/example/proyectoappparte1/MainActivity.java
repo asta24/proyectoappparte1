c5a7ea15d1eaf730fa03d39b989f32bb1ca4b1f7
asta24 <alejandropepe862@gmail.com>
2023-04-24 12:25:15 +0200

    silueta menu brazos menu etc

--- app/src/main/java/com/example/proyectoappparte1/MainActivity.java
+++ app/src/main/java/com/example/proyectoappparte1/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -19,10 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn_avatar1 = (ImageButton) findViewById(R.id.btn_avatar1);
         btn_avatar2 = (ImageButton) findViewById(R.id.btn_avatar2);
-        btn_avatar3 = (ImageButton) findViewById(R.id.btn_avatar3);
-        btn_avatar4 = (ImageButton) findViewById(R.id.btn_avatar4);
-        btn_avatar5 = (ImageButton) findViewById(R.id.btn_avatar5);
-        btn_avatar6 = (ImageButton) findViewById(R.id.btn_avatar6);
+        btn_avatar3 = (ImageButton) findViewById(R.id.btn_avatar5);
+        btn_avatar4 = (ImageButton) findViewById(R.id.btn_avatar6);
+        btn_avatar5 = (ImageButton) findViewById(R.id.btn_avatar4);
+        btn_avatar6 = (ImageButton) findViewById(R.id.btn_avatar3);
         et_usuario = (EditText) findViewById(R.id.et_usuario);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launchermamado);
@@ -30,33 +29,39 @@ public class MainActivity extends AppCompatActivity {
     public void usuario1 (View view){
         Intent i = new Intent(this, MenuPrincipal. class);
         i.putExtra("dato",et_usuario.getText().toString());
+        i.putExtra("avatar", R.drawable.av1);
         startActivity(i);
 
     }
     public void usuario2 (View view) {
         Intent i = new Intent(this, MenuPrincipal. class);
         i.putExtra("dato", et_usuario.getText().toString());
+        i.putExtra("avatar", R.drawable.av2);
         startActivity(i);
     }
     public void usuario3 (View view) {
         Intent i = new Intent(this, MenuPrincipal. class);
         i.putExtra("dato", et_usuario.getText().toString());
+        i.putExtra("avatar", R.drawable.av3);
         startActivity(i);
     }
     public void usuario4 (View view) {
         Intent i = new Intent(this, MenuPrincipal. class);
         i.putExtra("dato", et_usuario.getText().toString());
+        i.putExtra("avatar", R.drawable.av4);
 
         startActivity(i);
     }
     public void usuario5 (View view) {
         Intent i = new Intent(this, MenuPrincipal. class);
         i.putExtra("dato", et_usuario.getText().toString());
+        i.putExtra("avatar", R.drawable.av5);
         startActivity(i);
     }
     public void usuario6 (View view) {
         Intent i = new Intent(this, MenuPrincipal. class);
         i.putExtra("dato", et_usuario.getText().toString());
+        i.putExtra("avatar", R.drawable.av6);
         startActivity(i);
     }
 
--- app/src/main/java/com/example/proyectoappparte1/MenuPrincipal.java
+++ app/src/main/java/com/example/proyectoappparte1/MenuPrincipal.java
@@ -2,14 +2,18 @@ package com.example.proyectoappparte1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MenuPrincipal extends AppCompatActivity {
+    private TextView tv_nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,24 @@ public class MenuPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_menu_principal);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launchermamado);
+        tv_nombre = (TextView) findViewById(R.id.tv_nombre);
+        int idImagen = getIntent().getIntExtra("avatar", -1);
+        if (idImagen != -1) {
+            ImageView imageView = findViewById(R.id.iv_pasar);
+            imageView.setImageResource(idImagen);
+        }
+        String dato = getIntent().getStringExtra("dato");
+        tv_nombre.setText(dato);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.overflow, menu);
         return true;
+
+    }
+    public void brazo (View view) {
+        Toast.makeText(this, "brazo lio", Toast.LENGTH_SHORT).show();
+
 
     }
     //Método para aherhar las acciones a todos los botones
