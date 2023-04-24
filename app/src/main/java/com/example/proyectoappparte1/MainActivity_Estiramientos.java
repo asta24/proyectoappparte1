bb74029c9c9ac761d9ab4d267a39abc8940f0770
carche8 <carche1987@gmail.com>
2023-04-24 19:24:46 +0200

    cambios strings y activity main_estiramientos (scroll view)

--- app/src/main/java/com/example/proyectoappparte1/MainActivity_Estiramientos.java
+++ app/src/main/java/com/example/proyectoappparte1/MainActivity_Estiramientos.java
@@ -3,6 +3,8 @@ package com.example.proyectoappparte1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity_Estiramientos extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class MainActivity_Estiramientos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_estiramientos);
+    }
+    public void Scrollview (View view) {
+
+        switch (view.getId()) {
+
+            case R.id.pechofondos24:
+                Toast.makeText(this, "pecho", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.brazobueno:
+                Toast.makeText(this, "brazo", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.piernafondo:
+                Toast.makeText(this, "piernas", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.espaldafondo1:
+                Toast.makeText(this, "espalda", Toast.LENGTH_SHORT).show();
+                break;
+
+
+
+        }
+
     }
 }
