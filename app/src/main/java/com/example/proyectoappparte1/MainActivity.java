32597795f8cd4c07f9ed354e89905ceb8d4307df
carche8 <carche1987@gmail.com>
2023-04-24 13:05:08 +0200

    pag principal cambiada

--- app/src/main/java/com/example/proyectoappparte1/MainActivity.java
+++ app/src/main/java/com/example/proyectoappparte1/MainActivity.java
@@ -7,24 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton btn_avatar1, btn_avatar2, btn_avatar3, btn_avatar4, btn_avatar5, btn_avatar6;
+    ImageView iv_avatar1, iv_avatar2, iv_avatar3, iv_avatar4, iv_avatar5, iv_avatar6;
     private EditText et_usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_avatar1 = (ImageButton) findViewById(R.id.btn_avatar1);
-        btn_avatar2 = (ImageButton) findViewById(R.id.btn_avatar2);
-        btn_avatar3 = (ImageButton) findViewById(R.id.btn_avatar5);
-        btn_avatar4 = (ImageButton) findViewById(R.id.btn_avatar6);
-        btn_avatar5 = (ImageButton) findViewById(R.id.btn_avatar4);
-        btn_avatar6 = (ImageButton) findViewById(R.id.btn_avatar3);
+        iv_avatar1 = (ImageView) findViewById(R.id.iv_avatar1);
+        iv_avatar2 = (ImageView) findViewById(R.id.iv_avatar2);
+        iv_avatar3 = (ImageView) findViewById(R.id.iv_avatar3);
+        iv_avatar4 = (ImageView) findViewById(R.id.iv_avatar4);
+        iv_avatar5 = (ImageView) findViewById(R.id.iv_avatar5);
+        iv_avatar6 = (ImageView) findViewById(R.id.iv_avatar6);
         et_usuario = (EditText) findViewById(R.id.et_usuario);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.ic_launchermamado);
+        getSupportActionBar().setIcon(R.mipmap.ic_launchermamado_background);
     }
     public void usuario1 (View view){
         Intent i = new Intent(this, MenuPrincipal. class);
