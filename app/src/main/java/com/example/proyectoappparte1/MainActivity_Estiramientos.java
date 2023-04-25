7ac4c9e2af8737f15bcee36b1c64efb2358f02cb
carche8 <carche1987@gmail.com>
2023-04-25 20:59:49 +0200

    Cambios strings en adicionales, adicionales 2. Cambios strings en estiramientos, estiramientos 2. Crear Estiramientos y Estiramientos 2. Cambios En Adicionales

--- app/src/main/java/com/example/proyectoappparte1/MainActivity_Estiramientos.java
+++ app/src/main/java/com/example/proyectoappparte1/MainActivity_Estiramientos.java
@@ -7,38 +7,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity_Adicionales extends AppCompatActivity {
+public class MainActivity_Estiramientos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_adicionales);
+        setContentView(R.layout.activity_main_activity_estiramientos);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launchermamado);
     }
 
 
-        public void Navegarpecho(View view){
+    public void Estiramientospecho(View view){
         Intent i = new Intent(this, MainActivityAdicionales_2.class);
         i.putExtra("sitioWeb", "https://www.fitnessintegral.org/rutina-de-pecho-gym/");
         startActivity(i);
     }
-        public void Navegarbrazo(View view){
+    public void Estiramientosbrazos(View view){
         Intent i = new Intent(this, MainActivityAdicionales_2.class);
         i.putExtra("sitioWeb", "https://www.vitonica.com/entrenamiento/rutina-para-entrenar-tus-brazos-al-completo-gimnasio");
         startActivity(i);
     }
-        public void Navegarpiernas(View view){
+    public void Estiramientospiernas(View view){
         Intent i = new Intent(this, MainActivityAdicionales_2.class);
         i.putExtra("sitioWeb", "https://www.winfitclubs.com/rutina-de-piernas-gym/");
         startActivity(i);
     }
-        public void Navegarespalda(View view){
+    public void Estiramientosespalda(View view){
         Intent i = new Intent(this, MainActivityAdicionales_2.class);
         i.putExtra("sitioWeb", "https://www.expansion.com/fueradeserie/cuerpo/2022/08/25/62d1384f468aebd5368b466d.html");
         startActivity(i);
     }
-        public void Navegarhombros(View view){
+    public void Estiramientoshombros(View view){
         Intent i = new Intent(this, MainActivityAdicionales_2.class);
         i.putExtra("sitioWeb", "https://www.expansion.com/fueradeserie/cuerpo/2022/07/13/62c3f6ee468aeb96728b45ef.html");
         startActivity(i);
---
+++ app/src/main/java/com/example/proyectoappparte1/MainActivity_Estiramientos_2.java
@@ -0,0 +1,31 @@
+package com.example.proyectoappparte1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity_Estiramientos_2 extends AppCompatActivity {
+    WebView wv_estiramientos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_estiramientos2);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launchermamado);
+
+        wv_estiramientos = (WebView) findViewById(R.id.wv_estiramientos);
+        String URL = getIntent().getStringExtra("sitioWeb");
+        if(!URL.startsWith("http://") && !URL.startsWith("https://")){
+            URL = "http://" + URL;
+        }
+        wv_estiramientos.loadUrl(URL);
+    }
+    public void Volver (View view){
+        finish();
+    }
+}
