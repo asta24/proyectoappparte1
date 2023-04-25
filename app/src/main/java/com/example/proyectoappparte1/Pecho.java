7e9a5b01c54e7c6232fd05f92c8a4829d44e2a95
asta24 <alejandropepe862@gmail.com>
2023-04-25 11:18:14 +0200

    cambio menu y creo activities

---
+++ app/src/main/java/com/example/proyectoappparte1/Pecho.java
@@ -0,0 +1,14 @@
+package com.example.proyectoappparte1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pecho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pecho);
+    }
+}
